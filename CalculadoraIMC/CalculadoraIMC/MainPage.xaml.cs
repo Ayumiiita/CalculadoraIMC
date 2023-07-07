cbf8ea5f834bc2c752decad1cc4dced14db50b54
Laura <Laura@DESKTOP-P4G2UL8>
2023-07-07 22:50:18 +0200

    Agregar archivos de proyecto.

---
+++ CalculadoraIMC/CalculadoraIMC/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalculadoraIMC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(Altura.Text) &&
+                    !string.IsNullOrEmpty(Peso.Text))
+            {
+                var altura = double.Parse(Altura.Text);
+                var peso = double.Parse(Peso.Text);
+
+                var imc = peso / (altura * altura);
+                IMC.Text = imc.ToString();
+
+                string resultado = "";
+
+                if (imc < 18.5)
+                {
+                    resultado = "Estás por debajo del peso";
+                }
+                else if (imc >= 18.5 && imc <= 24.9)
+                {
+                    resultado = "Tu IMC es saludable";
+                }
+                else if (imc >= 25 && imc <= 29.9)
+                {
+                    resultado = "Tienes sobrepeso";
+
+                }
+                else
+                {
+                    resultado = "Tienes obesidad ¡Cuídate!";
+                }
+
+                DisplayAlert("Resultado", resultado, "OK");
+            }
+            else
+            {
+                DisplayAlert("Datos incorrectos", "Por favor introduce tus datos", "OK");
+            }
+    
+    }
+    }
+}
+
